95b7d425fe110ae1ffc896c14d28ae3f7e9966fe
Naeem Malik <nakram@mail-dev.tpinteractive.com>
2021-05-30 22:25:14 +0500

    Added student value class

---
+++ Values/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoQAUITestAutomation.Values
+{
+    class Student
+    {
+        private string _firsName;
+        private string _lastName;
+        private string _mobile;
+        private PagePractice.FormGender _gender;
+
+        public static Student Create(string firstName, string lastName, string mobile, PagePractice.FormGender gender)
+        {
+            return new Student(firstName, lastName, mobile, gender);
+        }
+        private Student(string firstName, string lastName, string mobile, PagePractice.FormGender gender)
+        {
+            _firsName = firstName;
+            _lastName = lastName;
+            _mobile = mobile;
+            _gender = gender;
+        }
+
+        private string First => _firsName;
+        private string Last => _lastName;
+        private string Mobile => _mobile;
+        private PagePractice.FormGender Gender => _gender;
+    }
+}
